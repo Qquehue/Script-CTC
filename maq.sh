d9005db7c3383b75978ad56e07a04323f004b9f3
Qquehue <alejandro.qquehue@sptech.school>
2022-11-30 02:13:37 -0300

    ajustes

--- maq.sh
+++ maq.sh
@@ -60,30 +60,45 @@ identificador INT
 
 echo "Faça uma conexão com interface gráfica, abra o terminal e digite: guiJava"
 else
+cd /home/ubuntu
 echo "Baixando aplicação versão CLI"
 sleep 2
-git clone https://Qquehue@github.com/Qquehue/CTC-LocalApp-CLI
+git clone https://github.com/Qquehue/CTC-LocalApp-CLI.git
 echo "instalando docker..."
 sleep 3
 
 sudo apt install docker.io -y
   sudo systemctl start docker
   sudo systemctl enable docker
-docker build -t dockerfile .
-docker run -d -p 3306:3306 --name bd-CTC -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_DATABASE=bd-CTC dockerfile
+  sudo docker pull mysql:8.0
+  sudo docker run -d -p 3306:3306 --name bd-CTC -e "MYSQL_DATABASE=bd-CTC" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:8.0
+  sudo docker exec -it bd-CTC mysql -u root -p -B -N -e"
+USE bd-CTC;
+
+create table usoMaquinaReal 
+(  
+idUso INT PRIMARY KEY AUTO_INCREMENT, 
+processos INT, 
+usoCPU DOUBLE,
+usoMemoria DOUBLE,
+upTime DATETIME, 
+identificador INT
+);"
 
 echo "Banco ctc criado"
 sleep 2
-cd arquivos-sh
-
-echo "Voce esta aqui:"
-pwd
-sleep 2
-
-alias cliJava="cd /home/ubuntu/arquivos-sh ; bash cliJava.sh"
-
+  
   cd /home/ubuntu/arquivos-sh
   sudo chmod 777 cliJava.sh
 
-echo "Digite o seguinte comando para inciar a aplicacao: cliJava"
+  cd /home/ubuntu/Script-CTC/java
+sudo docker build -t dockerfile .
+sudo docker run -d -t --name java-CTC dockerfile
+
+cd /homr/ubuntu/CTC-LocalApp-CLI/API/local-app/target
+echo "Voce esta aqui:"
+pwd
+sleep 2
+echo "Digite o seguinte comando para inciar a aplicacao: bash cliJava.sh"
+java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 fi
